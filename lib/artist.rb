1974338d15e0f76a2c12a7278734729b2096b6de
Nico Bazzoni <you@example.com>
2020-12-06 16:37:18 -0500

    commit tree b1ade40b4fc0c2abf9888a26b9db8a322a4bf9d5

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,16 @@
 require 'pry'
 
 class Artist
+   extend Memorable::ClassMethods 
+   include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
+    self.class.all << self
     @songs = []
   end
 
@@ -19,13 +22,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,14 @@
 require 'pry'
 
 class Song
+  extend Memorable::ClassMethods 
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
   def initialize
-    @@songs << self
+    self.class.all << self
   end
 
   def self.find_by_name(name)
@@ -18,13 +19,13 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
